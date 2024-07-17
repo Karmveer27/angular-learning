178ea92c4bf8d71eb3b3ace61b942ca62a17b96d
Karmveer Singh <karmveer.nz@gmail.com>
2024-07-18 00:19:55 +1200

    Added mock reviews and review interface

---
+++ angular-frontend/src/data/mock-reviews.ts
@@ -0,0 +1,29 @@
+import { Review } from "./Reviews"
+export const REVIEWS: Review[]=[
+    {
+        id: 1,
+        rating: 4,
+        text: "Great Service, would come by again",
+        name: "Bruce Wayne"
+    },
+    {
+        id: 2,
+        rating: 5,
+        text: "Customer Support was perfect",
+        name: "Cristiano Ronaldo"
+    },
+    {
+        id: 3,
+        rating: 4,
+        text: "I am Chris Bumsted!",
+        name: "Chris Bumsted"
+    },
+    {
+        id: 4,
+        rating: 3,
+        text: "Enjoyed my experience with Simpro",
+        name: "Lionel Messi"
+    },
+
+
+]
