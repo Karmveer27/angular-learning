1eb25d847ade3feef3b008a855c599a7c4ed83ee
Karmveer Singh <karmveer.nz@gmail.com>
2024-07-18 01:06:02 +1200

    Finished working on body of page which includes reviews and review components

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { LinkComponent } from './components/link/link.component';
+import { ReviewsComponent } from './components/reviews/reviews.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavComponent,LinkComponent],
+  imports: [RouterOutlet,NavComponent,LinkComponent,ReviewsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
